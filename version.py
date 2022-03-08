32dc875683f77fd216c525155b53e3854ae3419b
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-08 22:29:11 +0530

    Add files via upload

---
+++ version.py
@@ -0,0 +1,7 @@
+#write a python program to get the version of the python
+#importing module "sys"
+import sys
+print("python version")
+print(sys.version)
+print("version info")
+print(sys.version_info)
